ed1cdf06e29d389cd2c99a515e60bedc68ed776d
Raidy Machado <raidy_silva@hotmail.com>
2020-07-16 18:05:15 -0400

    initial

---
+++ ExampleModule.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyRestAPI
+{
+    public class ExampleModule : NancyModule
+    {
+        public ExampleModule() : base("/api")
+        {
+            Get["/"] = Index;
+        }
+
+        //Get - http://localhost:44372:api/
+        private dynamic Index(dynamic ars)
+        {
+            return "Welcome sample module. Using Nancy 1.4.4";
+        }
+    }
+}
